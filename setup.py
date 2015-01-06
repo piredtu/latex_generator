1d4158cebc41d370de0fde54b93dc9cf74d31363
Pierre-Elouan Réthoré <rethore@gmail.com>
2015-01-06 10:41:20 +0100

    initial setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='latex_generator',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/piredtu/latex_generator',
+    license='AGPL v3.0',
+    author='Pierre-Elouan Rethore',
+    author_email='pire@dtu.dk',
+    description='A report and slide generator using python and latex'
+)
